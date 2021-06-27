579ee55650b6fe9746964f59b51347d059ff5f65
c-zw <a19575091@gmail.com>
2021-06-27 09:43:33 +0800

    fix

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -4,7 +4,7 @@ declare global {
     namespace Express {
         interface Request {
             //@ts-ignore
-            user: {
+            user?: {
                 id: string;
                 username: string;
                 createTime: string
